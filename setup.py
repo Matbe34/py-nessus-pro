f86b4fa3573a2b26c9cb4b4e79c2e5f8049c61da
Matbe34 <ferranmabe@gmail.com>
2025-03-25 14:23:30 +0100

    version pump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_nessus_pro',
-    version='1.2.9',
+    version='1.3.0rc1',
     packages=find_packages(),
     install_requires=[
          'selenium',
